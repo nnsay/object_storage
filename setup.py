60568f18c6ed170944ebee3488b63440b9855e94
Jimmy Wang <jimmy.w@aliyun.com>
2024-04-25 16:18:01 +0800

    debug: so package

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="object_storage",
     version="0.1",
-    packages=["object_storage"],  # 表示当前目录作为根包
+    packages=find_packages(),  # 表示当前目录作为根包
     package_data={
         "object_storage": ["object_storage/object_storage.so"]
     },  # 确保 .so 文件被包括
