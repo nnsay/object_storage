239fbd93c128adb3f58d6134853e5886897d6bac
Jimmy Wang <jimmy.w@aliyun.com>
2024-04-25 15:18:19 +0800

    test: add setup

--- setup.py
+++ setup.py
@@ -4,7 +4,9 @@ setup(
     name="object_storage",
     version="0.1",
     packages=[""],  # 表示当前目录作为根包
-    package_data={"": ["object_storage.so"]},  # 确保 .so 文件被包括
+    package_data={
+        "object_storage": ["object_storage/object_storage.so"]
+    },  # 确保 .so 文件被包括
     include_package_data=True,
     zip_safe=False,
 )
