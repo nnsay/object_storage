3c184beffdee7801c1e573775418049dfed0b0bb
Jimmy Wang <jimmy.w@aliyun.com>
2024-04-25 15:09:17 +0800

    test: add setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="object_storage",
     version="0.1",
-    packages=find_packages(),
+    packages=[],
     package_data={"": ["object_storage.so"]},
     include_package_data=True,
     zip_safe=False,
