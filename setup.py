38e6aea421c313db26cebb8c1fa79436699f1dee
Jimmy Wang <jimmy.w@aliyun.com>
2024-04-25 16:27:55 +0800

    debug: so package

--- setup.py
+++ setup.py
@@ -4,6 +4,11 @@ setup(
     name="object_storage",
     version="0.1",
     packages=find_packages(),  # 表示当前目录作为根包
-    package_data={"": ["/*.so"]},  # 确保 .so 文件被包括
+    package_data={
+        "": [
+            "object_storage.cpython-312-darwin.so",
+            "object_storage.so",
+        ]
+    },  # 确保 .so 文件被包括
     include_package_data=True,
 )
