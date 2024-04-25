9756205adf6986b082433316a913b03861f07c1c
Jimmy Wang <jimmy.w@aliyun.com>
2024-04-25 18:10:30 +0800

    debug: so package

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name="object_storage",
     version="0.1",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     package_data={
-        "": [
-            "object_storage.cpython-312-darwin.so",
-            "object_storage.so",
+        "object_storage": [
+            "*.so",
         ]
     },  # 确保 .so 文件被包括
     include_package_data=True,
-    zip_safe=False
+    zip_safe=False,
 )
